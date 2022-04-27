c37a37bbec22efc090d332ed76d6d2d3a35f7ad5
TheNewTimeGamer <32019721+TheNewTimeGamer@users.noreply.github.com>
2022-04-28 00:26:45 +0200

    Initial push, some basic car stuff.

---
+++ case_one/car.php
@@ -0,0 +1,81 @@
+<?php
+
+// This class contains properties for a car.
+class CarProperties {
+
+    public $name;
+    public $model;
+    public $color;
+    public $maxSpeed;
+    public $acceleration;
+
+    public $powerplant;
+    public $drive;
+
+    /**
+     * @param $name Name/Identification of the car.
+     * @param $model Model of the car.
+     * @param $color Color of the car.
+     * @param $maxSpeed Maximum speed of the car in m/s.
+     * @param $acceleration Acceleration of the car in m/s².
+     * @param $powerplant Powerplant of the car (Diesel, Gasoline, Electric, Hydrogen).
+     * @param $drive Drive of the car. (FWD, RWD, AWD)
+     */    
+    public function __construct($name, $model, $color, $maxSpeed, $acceleration, $powerplant, $drive) {
+        $this->name = $name;
+        $this->model = $model;
+        $this->color = $color;
+        $this->maxSpeed = $maxSpeed;
+        $this->acceleration = $acceleration;
+        $this->powerplant = $powerplant;
+        $this->drive = $drive;
+    }
+
+}
+
+// This class is used to represent a car.
+class Car {
+
+    public $properties;
+
+    private $currentSpeed;
+    private $angle;
+
+    public function __construct($carProperties) {
+        $this->properties = $properties;
+    }
+    
+    /**
+     * @return The distance it takes for the car to come to a full stop in meters.
+     */
+    public function getBrakeDistance() {
+        return ($this->currentSpeed / 10) * ($this->currentSpeed / 10);
+    }
+
+    /**
+     * @param $degrees The angle in degrees to turn (Clockwise orientation).
+     * @return The new angle of the car.
+     */
+    public function turn($degrees) {
+        $this->angle = $this->angle + $degrees;
+        return $this->angle;
+    }
+
+    /**
+     * @param $time for how long the car needs to accelerate in seconds.
+     * @return An array containing the new speed of the car and the distance traveled.
+     */
+    public function accelerate($time) {
+        $timeTillTopSpeed = ($this->properties->maxSpeed - $this->currentSpeed) / $this->properties->acceleration;  // Get time till max speed is achieved.
+        $deltaTime = 0;
+        if($time > $timeTillTopSpeed) {
+            $deltaTime = $time-$timeTillTopSpeed;                                                                   // Calculate the remaining time the accelerator is held down.
+            $time = $timeTillTopSpeed;
+        }
+        $this->currentSpeed = $this->currentSpeed + $this->properties->acceleration * $time;                        // Calculate the new speed.
+        $distance = ($this->currentSpeed * $time) + 0.5*($this->properties->acceleration * ($time * $time));        // Calculate distance traveled while accelerating.
+        $distance = $distance + ($deltaTime * $this->properties->maxSpeed);                                         // Add remaining distance traveled while at max speed.
+        return array("speed"=>$this->currentSpeed, "distance"=>$distance);
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/landing.css" type="text/css">
+    <title>Tasklist</title>
+</head>
+<body>
+    <ul>
+        <a href="case_one"><li>Case one</li></a>
+        <a href="case_two"><li>Case two</li></a>
+    </ul>
+</body>
+</html>
